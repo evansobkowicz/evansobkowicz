a1d9a33c94e68409653779f314e1aa8b864ad10c
Evan Sobkowicz <evansobkowicz@gmail.com>
2021-01-02 06:24:15 -0500

    upload changes

--- main.js
+++ main.js
@@ -3,6 +3,8 @@
 const TimelineElement = document.getElementById('timeline');
 const TweetsElement = document.getElementById('tweets');
 const TweetsLoadingElement = document.getElementById('tweets-loading');
+const MainElement = document.getElementById('main');
+const MetaElement = document.getElementById('meta');
 
 const TimelineOpts = {
   chrome: 'noheader nofooter noborders transparent noscrollbar',
@@ -12,22 +14,28 @@ const TimelineOpts = {
   tweetLimit: 20
 };
 
-const TweetOpts = { align: 'center', conversation: 'none', dnt: true }
+const TweetOpts = { align: 'center', conversation: 'none', dnt: true };
 
 const limit = 6;
 const screenName = 'evansobkowicz';
+const includeRTs = false;
 
-const isTimelineEvent = event => event.target.title === 'Twitter Timeline';
+const isTimelineEvent = ({ title }) => title === 'Twitter Timeline';
+const isOriginalTweet = ({ dataset: { renderedTweetId, tweetId } }) => renderedTweetId === tweetId;
 
 twttr.ready(twttr => {
   twttr.widgets.createTimeline({ sourceType: 'profile', screenName }, TimelineElement, TimelineOpts);
-  twttr.events.bind('rendered', event => {
-    if (!isTimelineEvent(event)) { return; }
-    const tweetEls = event.target.contentDocument && event.target.contentDocument.querySelectorAll('.timeline-Tweet');
-    const nonRetweetEls = Array.from(tweetEls).filter(el => el.dataset.tweetId === el.dataset.renderedTweetId);
-    const tweetIds = nonRetweetEls.slice(0, limit).map(el => el.dataset.tweetId);
+  twttr.events.bind('rendered', ({ target }) => {
+    if (!isTimelineEvent(target)) { return; }
+    const tweetEls = Array.from(target.contentDocument && target.contentDocument.querySelectorAll('.timeline-Tweet'));
+    const filteredEls = includeRTs ? tweetEls : tweetEls.filter(isOriginalTweet);
+    const tweetIds = filteredEls.slice(0, limit).map(el => el.dataset.tweetId);
     const tweets = tweetIds.map(id => twttr.widgets.createTweet(id, TweetsElement, TweetOpts));
-    Promise.resolve(tweets[0]).then(() => TweetsLoadingElement.remove());
+    Promise.all(tweets).then(() => {
+      TweetsLoadingElement.remove();
+      MetaElement.style = '';
+      MainElement.appendChild(MetaElement);
+    });
   });
 });
 
